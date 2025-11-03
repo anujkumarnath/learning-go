035898d50c83dd92669db66249ebf37ab4b2a05c
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-11-03 07:53:50 +0530

    goroutine with unbuffered channel

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
-func main() {
-	fmt.Println("Before invoking goroutine")
-
-	go func() {
-		fmt.Println("Goroutine running...");
-	}()
-
-	/* otherwise the program will exit before the go routine finishes executing */
-	fmt.Println("Sleeping for second...");
-	time.Sleep(1 * time.Second)
-
-	fmt.Println("Before invoking goroutine")
+func goroutine(channel chan string) {
+	fmt.Println("I am a goroutine that communicates via a channel")
+	channel <- "Done running go routine."
 }
 
+func main() {
+	/* unbuffered channel - the read/write is synchronous
+	 * i.e., the writer will be paused until the reader reads the message
+	 */
+	channel := make(chan string)
+	go goroutine(channel)
+	msg := <- channel
+
+
+	/*
+		// Uncomment to see the wrong "Sleep" way to wait for goroutines
+		fmt.Println("Before running goroutine")
+
+		go func() {
+			fmt.Println("Goroutine running...");
+		}()
+
+		// otherwise the program will exit before goroutine finishes executing
+		fmt.Println("Sleeping for second for goroutine ...");
+		time.Sleep(1 * time.Second)
+		fmt.Println("After running Goroutine 1")
+	*/
+
+	fmt.Println(msg)
+}
 
