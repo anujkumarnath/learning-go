916b2a67cf840486f7c569f440743fed71d47efa
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-11-16 22:07:42 +0530

    non-blocking channel operations

---
+++ non-blocking-channel-operations.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no messages received")
+	}
+
+	msg := "Hi"
+
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	case signals <- true:
+		fmt.Println("sent signal", true)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
