db8df5be38b9f521ce6697bf17876dd8f50de0ee
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-11-16 23:14:31 +0530

    learned about tickers

---
+++ ticker.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("Tick at", t)
+			}
+		}
+	}()
+
+	time.Sleep(6 * time.Second)
+	ticker.Stop()
+	close(done)
+	fmt.Println("Ticker stopped")
+}
