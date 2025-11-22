42a50fc88a0215c0fe3bfa36a119539f011990d3
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-11-22 20:36:34 +0530

    learned about http client

---
+++ http-client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	fmt.Println("Response status:", res.Status)
+
+	scanner := bufio.NewScanner(res.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
