1a7022e7ff73acb8784898d49f2a12991f80b8d9
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-11-24 09:28:56 +0530

    basic error handling

--- modules-and-packages/greetings/greetings.go
+++ modules-and-packages/greetings/greetings.go
@@ -1,10 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // a function whose name starts with a capital letter
 // can be called from code not belonging to the same package
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi %v! Welcomei!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf("Hi %v! Welcome!", name)
+	return message, nil
 }
