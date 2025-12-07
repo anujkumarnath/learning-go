c5d8ed5d53c20d6e39db31cd3148f344625ea2d2
Anuj Kumar Nath <anujkumarnath@gmail.com>
2025-12-07 10:30:19 +0530

    done 8 bytes

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"io"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	check(err)
+
+	buffer := make([]byte, 8)
+
+	for {
+		readBytes, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		} else {
+			check(err)
+		}
+		fmt.Printf("read: %s\n", buffer[:readBytes])
+	}
 }
